b0c30b37359a3b47ecbdd166df3ed0fafdd5249e
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-03-25 01:11:40 -0500

    first commit

---
+++ client/pages/index.tsx
@@ -0,0 +1,18 @@
+import React, {useEffect, useState} from 'react'
+
+function index() {
+
+    useEffect(() =>{
+      fetch("http://localhost:8080/api")
+      .then(response => response.json())
+      .then(data => {console.log(data)
+        });
+    }, [])
+
+
+    return (
+    <div>index</div>
+  )
+}
+
+export default index
