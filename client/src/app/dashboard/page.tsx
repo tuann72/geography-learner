f7fa1ae7316513a91a4f66112494f15961f20269
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-04-10 11:50:43 -0500

    Created onClick event to change page.

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,11 @@
+'use client'
 import * as React from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Page() {
+
+    const router = useRouter();
+
     return (
     <div className="bg-white flex flex-col h-screen justify-center items-center">
         <div className="h-1/2 flex flex-col justify-center items-center">
@@ -8,8 +13,8 @@ export default function Page() {
             <h2 className="text-black">Select a learning game below!</h2>
         </div>
         <div className="grid grid-cols-2 w-2/3 h-1/2 gap-10">
-            <button className="h-1/2 bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">Button</button>
-            <button className="h-1/2 bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">Button</button>
+            <button onClick={() => router.push("/dashboard/flags")} className="text-xl h-1/2 bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">Flag Game</button>
+            <button className="text-xl h-1/2 bg-blue-500 hover:bg-blue-700 py-2 px-4 rounded">Button</button>
         </div>
     </div>
     )
