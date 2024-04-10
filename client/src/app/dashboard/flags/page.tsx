87f37444a004da8a615850bbd3cabb43c84e6c0d
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-04-10 13:34:33 -0500

    Create flag game page. Added basic parts.

--- client/src/app/dashboard/flags/page.tsx
+++ client/src/app/dashboard/flags/page.tsx
@@ -20,14 +20,31 @@ export default function Page() {
 
 
     return (
-      <div>
+      <div className="bg-sky-50 h-screen flex flex-col items-center">
+        <div className="text-black text-3xl h-1/5 flex flex-col justify-end items-center py-10">
+          Flag Game
+        </div>
+        <div className="text-black h-2/5  bg-yellow-200">
+          Display
+        </div>
+        <div className="text-black h-2/5 flex flex-col items-center">
+          <div className="flex flex-col items-center pb-4">
+            <label>Enter the name of the country that matches the flag!</label>
+            <input type="text" id="country_entry" placeholder="Country" className="bg-gray-50 border border-gray-300 w-full h-10 rounded-lg px-2"/>
+          </div>
+          <div className="grid grid-cols-3 gap-3">
+            <button className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Submit</button>
+            <button className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Skip</button>
+            <button className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Home</button>
+          </div>
+        </div>
           {
-            countryImg.map((link, index)=>(
-              <div key={index}>
-                <img src={link}/>
-                <div>{countries[index]}</div>
-              </div>
-            ))
+            // countryImg.map((link, index)=>(
+            //   <div key={index}>
+            //     <img src={link}/>
+            //     <div>{countries[index]}</div>
+            //   </div>
+            // ))
           }
       </div>
   );
