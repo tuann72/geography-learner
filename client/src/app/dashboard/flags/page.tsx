27ee8e98a3e1adce02dc1b597ada75c45830a084
Tuan Nguyen <tuannguyen08426@gmail.com>
2024-04-12 02:03:56 -0500

    Created a util module called GetRandomNumber to retrieve a random index. Additionally integrated the image changing with the index.

--- client/src/app/dashboard/flags/page.tsx
+++ client/src/app/dashboard/flags/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import React, {useEffect, useState} from 'react'
+import GetRandomNum from '@/utils/randomNum'
 
 export default function Page() {
 
+    const intialIndex = GetRandomNum(0, 233)
     const [countries, setCountries] = useState([])
     const [countryImg, setCountryImg] = useState([])
+    const [index, setIndex] = useState(intialIndex)
+    const [score, setScore] = useState(0)
 
     useEffect(() =>{
       fetch("http://localhost:8080/api/flags")
@@ -20,12 +24,14 @@ export default function Page() {
 
 
     return (
+      
       <div className="bg-sky-50 h-screen flex flex-col items-center">
-        <div className="text-black text-3xl h-1/5 flex flex-col justify-end items-center py-10">
-          Flag Game
+        <div className="text-black h-1/5 flex flex-col justify-end items-center py-10">
+          <h1 className="text-3xl">Flag Game</h1>
+          <h2>Score: {score}</h2>
         </div>
-        <div className="text-black h-2/5  bg-yellow-200">
-          Display
+        <div className="h-2/5">
+          <img className="h-64 w-100" src={countryImg[index]}/>
         </div>
         <div className="text-black h-2/5 flex flex-col items-center">
           <div className="flex flex-col items-center pb-4">
@@ -33,8 +39,12 @@ export default function Page() {
             <input type="text" id="country_entry" placeholder="Country" className="bg-gray-50 border border-gray-300 w-full h-10 rounded-lg px-2"/>
           </div>
           <div className="grid grid-cols-3 gap-3">
-            <button className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Submit</button>
-            <button className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Skip</button>
+            {/* The onClick event creates a arrow function that calls setIndex
+                setIndex uses useState to update the index state with a random number
+                This updates any component that uses the index state.
+            */}
+            <button onClick={() => setScore(1)} className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Submit</button>
+            <button onClick={() => setIndex(GetRandomNum(0, 233))} className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Skip</button>
             <button className="text-l text-white bg-cyan-700 hover:bg-cyan-950 px-4 py-2 rounded-lg border border-gray-300">Home</button>
           </div>
         </div>
---
+++ client/src/utils/randomNum.tsx
@@ -0,0 +1,5 @@
+const GetRandomNum = (min : number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export default GetRandomNum;
